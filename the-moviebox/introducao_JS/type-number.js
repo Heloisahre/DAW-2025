cd6a3feaf05bbc8e074bfa6b55d5b7fc68d51119
Heloisahre <168488760+Heloisahre@users.noreply.github.com>
2025-10-20 21:36:27 -0300

    aula react

---
+++ the-moviebox/introducao_JS/type-number.js
@@ -0,0 +1,14 @@
+const primeiroNro = 3;
+const segundoNro = 5;
+
+const resultado = primeiroNro + segundoNro;
+console.log(resultado);
+
+console.log(`${primeiroNro} + ${segundoNro}`);
+
+console.log(`${primeiroNro} + ${segundoNro} = ${primeiroNro + segundoNro}`);
+
+
+var terceiroNro = 15.50
+
+terceiroNro = 'abacate';
