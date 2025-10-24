ac33153083bffedfe80d2e0aa4a94ee3805a4023
Heloisahre <168488760+Heloisahre@users.noreply.github.com>
2025-10-23 21:58:31 -0300

    2° dia react

--- Projeto_TheMovieBox_React/src/App.tsx
+++ Projeto_TheMovieBox_React/src/App.tsx
@@ -1,9 +1,12 @@
+import { Footer } from "./components/layout/footer"
+import { Header } from "./components/layout/layout/header"
 
 function App() {
-
   return (
     <>
-    
+      <Header />
+      <main> </main>
+      <Footer />
     </>
   )
 }
