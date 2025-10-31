69a0ac3d1ead261889cc27e0b5cfbd8b672c0285
Heloisahre <168488760+Heloisahre@users.noreply.github.com>
2025-10-30 21:58:03 -0300

    3° etapa do projeto react

--- Projeto_TheMovieBox_React/src/App.tsx
+++ Projeto_TheMovieBox_React/src/App.tsx
@@ -1,11 +1,16 @@
+import { MovieContent } from "./cases/movies/components/movie-content"
 import { Footer } from "./components/layout/footer"
 import { Header } from "./components/layout/layout/header"
+import { Navbar } from "./components/layout/layout/navbar"
 
 function App() {
   return (
     <>
       <Header />
-      <main> </main>
+      <main> 
+        <Navbar />
+        <MovieContent/>
+      </main>
       <Footer />
     </>
   )
